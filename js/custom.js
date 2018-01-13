dd989c90bb0efaea20c7bb9855417bc1bfc8e8b8
OleksandrNazarenko112 <nazarenkovideo@gmail.com>
2018-01-13 20:13:23 +0200

    form-markup-and-scripts

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	
+	$('#fineness').selectric();
+	$('#loan-plans').selectric();
+	$('#discount').selectric();
+	$('.tooltip').tooltipster();
+
+	$("#stones-and-inserts").change(function() {
+		if ($(this).prop('checked') == true) {
+			$('.additional-conditions-container').animate({
+				opacity: 0
+			}, 500);
+		} else {
+			$('.additional-conditions-container').animate({
+				opacity: 1
+			}, 500);
+		}
+	});
+
+	var weight;
+	$('#weight').on('keyup', function() {
+		weight = parseInt($('#weight').val());
+	})
+	$('#weight').blur(function() {
+		$('#weight').val(weight + ' г');
+	});
+});
